75047cf4985499472d1a253c4f89c99e102f9a76
BeeUser12 <rosalina129@outlook.com>
2022-06-19 18:16:48 +0800

    Commit, Change Language

---
+++ Alchemy Tech Research/Window/MainForm.Designer.vb
@@ -0,0 +1,218 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FilesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewResearchToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.LoadToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveAsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CopyrightLabel = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.GameTitlePanel = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.TimerProgress = New System.Windows.Forms.Timer(Me.components)
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.MenuStrip1.SuspendLayout()
+        Me.GameTitlePanel.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FilesToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1004, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FilesToolStripMenuItem
+        '
+        Me.FilesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewResearchToolStripMenuItem, Me.ToolStripSeparator1, Me.LoadToolStripMenuItem, Me.SaveToolStripMenuItem, Me.SaveAsToolStripMenuItem})
+        Me.FilesToolStripMenuItem.Name = "FilesToolStripMenuItem"
+        Me.FilesToolStripMenuItem.Size = New System.Drawing.Size(42, 20)
+        Me.FilesToolStripMenuItem.Text = "Files"
+        '
+        'NewResearchToolStripMenuItem
+        '
+        Me.NewResearchToolStripMenuItem.Name = "NewResearchToolStripMenuItem"
+        Me.NewResearchToolStripMenuItem.Size = New System.Drawing.Size(184, 22)
+        Me.NewResearchToolStripMenuItem.Text = "New Progress"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(181, 6)
+        '
+        'LoadToolStripMenuItem
+        '
+        Me.LoadToolStripMenuItem.Name = "LoadToolStripMenuItem"
+        Me.LoadToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.O), System.Windows.Forms.Keys)
+        Me.LoadToolStripMenuItem.Size = New System.Drawing.Size(184, 22)
+        Me.LoadToolStripMenuItem.Text = "Load..."
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.ShortcutKeys = CType((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(184, 22)
+        Me.SaveToolStripMenuItem.Text = "Save..."
+        '
+        'SaveAsToolStripMenuItem
+        '
+        Me.SaveAsToolStripMenuItem.Name = "SaveAsToolStripMenuItem"
+        Me.SaveAsToolStripMenuItem.ShortcutKeys = CType(((System.Windows.Forms.Keys.Control Or System.Windows.Forms.Keys.Shift) _
+            Or System.Windows.Forms.Keys.S), System.Windows.Forms.Keys)
+        Me.SaveAsToolStripMenuItem.Size = New System.Drawing.Size(184, 22)
+        Me.SaveAsToolStripMenuItem.Text = "Save as"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Semibold", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(358, 45)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Alchemy Tech Research"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'CopyrightLabel
+        '
+        Me.CopyrightLabel.AutoSize = True
+        Me.CopyrightLabel.Location = New System.Drawing.Point(17, 468)
+        Me.CopyrightLabel.Name = "CopyrightLabel"
+        Me.CopyrightLabel.Size = New System.Drawing.Size(138, 15)
+        Me.CopyrightLabel.TabIndex = 3
+        Me.CopyrightLabel.Text = "© 2016-2022 Unreal_Neo"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(557, 458)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(62, 34)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Help"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(625, 458)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(62, 34)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Load"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(693, 458)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(108, 34)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "New Progress"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'GameTitlePanel
+        '
+        Me.GameTitlePanel.Controls.Add(Me.PictureBox1)
+        Me.GameTitlePanel.Controls.Add(Me.Label1)
+        Me.GameTitlePanel.Controls.Add(Me.CopyrightLabel)
+        Me.GameTitlePanel.Controls.Add(Me.Button3)
+        Me.GameTitlePanel.Controls.Add(Me.Button2)
+        Me.GameTitlePanel.Controls.Add(Me.Button1)
+        Me.GameTitlePanel.Location = New System.Drawing.Point(904, 79)
+        Me.GameTitlePanel.Name = "GameTitlePanel"
+        Me.GameTitlePanel.Size = New System.Drawing.Size(816, 504)
+        Me.GameTitlePanel.TabIndex = 7
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(249, 119)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(320, 240)
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'TimerProgress
+        '
+        Me.TimerProgress.Enabled = True
+        Me.TimerProgress.Interval = 10
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(0, 27)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1004, 640)
+        Me.Panel1.TabIndex = 8
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(96.0!, 96.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi
+        Me.ClientSize = New System.Drawing.Size(1004, 666)
+        Me.Controls.Add(Me.GameTitlePanel)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "MainForm"
+        Me.Text = "Alchemy Tech Research"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.GameTitlePanel.ResumeLayout(False)
+        Me.GameTitlePanel.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FilesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewResearchToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents LoadToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveAsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CopyrightLabel As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents GameTitlePanel As Panel
+    Friend WithEvents TimerProgress As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel1 As Panel
+End Class
---
+++ Alchemy Tech Research/Window/MainForm.vb
@@ -0,0 +1,67 @@
+﻿Imports System
+Imports System.IO
+Public Class MainForm
+
+    'Boolean Data
+    Dim progress As Boolean
+    Dim isHaveSave As Boolean
+
+    Dim AnimationFrame As UInteger
+
+    'Project Workspace Folder
+    Dim IniPathFolder As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) & "\WinFormGame\Alchemy Tech Research"
+    Dim SaveFileFolder As String = Environment.CurrentDirectory & "\Saves"
+
+    Private Sub MsgShow(Text As String, typ As Integer)
+        MsgBox(Text, typ, Me.Text)
+    End Sub
+    Private Sub CheckDirectory(ByVal DName As String)
+        If Not Directory.Exists(DName) Then
+            Directory.CreateDirectory(DName)
+        End If
+    End Sub
+    Private Sub DefaultIni()
+        If File.Exists(IniPathFolder & "\Config.ini") Then
+        Else
+            Dim fs As FileStream
+            fs = File.Create(IniPathFolder & "\Config.ini")
+            fs.Close()
+            writeINI(IniPathFolder & "\Config.ini", "Options", "Language", "en_us")
+        End If
+    End Sub
+    '---Program Start---
+    Private Sub FilesToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FilesToolStripMenuItem.Click
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckDirectory(IniPathFolder)
+        CheckDirectory(SaveFileFolder)
+        DefaultIni()
+        GameTitlePanel.Location = New Point(0, 27)
+        Me.Size = New Point(832, 572)
+    End Sub
+    Private Sub NewProgress(sender As Object, e As EventArgs) Handles NewResearchToolStripMenuItem.Click, Button3.Click
+        Me.Size = New Point(1020, 705)
+        isHaveSave = True
+    End Sub
+
+    Private Sub TimerProgress_Tick(sender As Object, e As EventArgs) Handles TimerProgress.Tick
+        If Not isHaveSave Then
+            LoadToolStripMenuItem.Enabled = False
+            SaveToolStripMenuItem.Enabled = False
+            SaveAsToolStripMenuItem.Enabled = False
+            GameTitlePanel.Visible = True
+        Else
+            LoadToolStripMenuItem.Enabled = False
+            SaveToolStripMenuItem.Enabled = False
+            SaveAsToolStripMenuItem.Enabled = False
+            GameTitlePanel.Visible = False
+        End If
+        If progress Then
+        End If
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
